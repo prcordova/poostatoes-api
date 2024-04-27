95170bf491c254bbe3173575a1ae110106ed8e63
prcordova <pedroricardocordova@yahoo.com.br>
2024-04-27 15:40:52 -0300

    fix:added CORS allow

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const secret = process.env.JWT_SECRET;
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://poostatoes.vercel.app"],
+    origin: "http://localhost:3000",
   })
 );
 app.use(express.json());
