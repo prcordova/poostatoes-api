8361caeee9a529cf51cc308c30bc0d06e32371e3
prcordova <pedroricardocordova@yahoo.com.br>
2024-04-28 20:24:00 -0300

    fix:adding cors to vercel domain

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://poostatoes.vercel.app"],
   })
 );
 
