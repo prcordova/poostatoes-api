52605978aa56d707a706c641b44c5cabf02990cf
prcordova <pedroricardocordova@yahoo.com.br>
2024-04-28 23:38:32 -0300

    fix: cors

--- index.js
+++ index.js
@@ -11,16 +11,37 @@ const multer = require("multer");
 require("dotenv").config();
 
 const allowedOrigins = [
-  "http://localhost:3000",
   "https://poostatoes.vercel.app",
   "https://poostatoes-api.vercel.app",
+  "http://localhost:3000",
 ];
+
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "A política de CORS para este site não " +
+          "permite acesso a partir do domínio especificado.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
+
+module.exports = (req, res) => {
+  //---- other code
+
+  //Preflight CORS handler
+  if (req.method === "OPTIONS") {
+    return res.status(200).json({
+      body: "OK",
+    });
+  }
+};
 
 const uploadMiddleware = multer({
   dest: "uploads/",
